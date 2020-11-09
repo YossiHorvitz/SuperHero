427104bf2ee02406cf7380b9f3736f771898cdad
Yossi <6804006@gmail.com>
2020-11-09 17:12:55 +0200

    Initial commit

---
+++ app/src/main/java/com/user/superhero/SecondFragment.kt
@@ -0,0 +1,12 @@
+package com.user.superhero
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class SecondFragment : Fragment(R.layout.fragment_second) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
